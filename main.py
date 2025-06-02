5dc4d84772dccb43ca52dd61ce2b3f712f028add
qwertyllionman <qwertyllionman@gmail.com>
2025-06-02 09:02:58 +0500

    run command changed

--- main.py
+++ main.py
@@ -455,4 +455,4 @@ async def trigger_report(current_user: User = Depends(get_current_user)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
